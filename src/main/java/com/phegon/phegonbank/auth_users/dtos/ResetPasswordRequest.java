747f32e8ec181410c83df5b4cc0369abacd03d6b
Sarayut <sarayut.sryst@gmail.com>
2026-02-19 22:16:05 +0700

    add PasswordResetCode dto

---
+++ src/main/java/com/phegon/phegonbank/auth_users/dtos/ResetPasswordRequest.java
@@ -0,0 +1,12 @@
+package com.phegon.phegonbank.auth_users.dtos;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ResetPasswordRequest {
+    private String email;
+    private String code;
+    private String newPassword;
+}
