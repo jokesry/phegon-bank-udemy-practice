a0789078e490f20fdbf8a421900f42020a41cc27
Sarayut <sarayut.sryst@gmail.com>
2026-02-22 23:16:35 +0700

    add LoginResponse dto

---
+++ src/main/java/com/phegon/phegonbank/auth_users/dtos/LoginResponse.java
@@ -0,0 +1,14 @@
+package com.phegon.phegonbank.auth_users.dtos;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
+public class LoginResponse {
+
+    @NotBlank(message = "Email is required")
+    @Email
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
