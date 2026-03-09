81545513caf3e38c275e605ca9013b9c78485779
Sarayut <sarayut.sryst@gmail.com>
2026-03-09 22:54:15 +0700

    add TransactionRequest DTO

---
+++ src/main/java/com/phegon/phegonbank/transaction/dtos/TransactionRequest.java
@@ -0,0 +1,28 @@
+package com.phegon.phegonbank.transaction.dtos;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.phegon.phegonbank.account.dtos.AccountDto;
+import com.phegon.phegonbank.enums.TransactionStatus;
+import com.phegon.phegonbank.enums.TransactionType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class TransactionRequest {
+
+    private TransactionType transactionType;
+    private BigDecimal amount;
+    private String accountNumber;
+    private String description;
+
+    private String destinationAccountNumber;
+
+}
